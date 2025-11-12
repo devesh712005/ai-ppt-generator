a124bf40b1fed7363d670a3d728f7632139ed71c
devesh <2k23.it1a.2310619@gmail.com>
2025-11-12 19:08:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button classname>Submit</Button>
+    </div>
+  );
+};
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Workspace from "./workspace/index.jsx";
+import Project from "./workspace/project/index.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/workspace",
+    element: <Workspace />,
+    children: [{ path: "project/:projectId", element: <Project /> }],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
